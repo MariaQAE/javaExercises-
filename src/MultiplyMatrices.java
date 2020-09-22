eeb32f25387e42554d3a63732ff7ea306a908939
mafanasieva <mariaafanasiev108@gmail.com>
2020-09-22 13:17:05 +0300

    Week 3. Arrays. Multiplying two 2 dimensional matrices.

--- src/MultiplyMatrices.java
+++ src/MultiplyMatrices.java
@@ -1,6 +1,6 @@
 public class MultiplyMatrices {
     public static void main(String args[]) {
-//creating two matrices
+
         int a[][] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
         int b[][] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
 
@@ -11,9 +11,9 @@ public class MultiplyMatrices {
                 multiplication[i][j] = 0;
                 for (int k = 0; k < 3; k++) {
                     multiplication [i][j] += a[i][k] * b[k][j];
-                }//end of k loop
-                System.out.print(multiplication[i][j] + " ");  //printing matrix element
-            }//end of j loop
+                }
+                System.out.print(multiplication[i][j] + " ");
+            }
             System.out.println();
         }
     }
